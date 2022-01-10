f90a84b9a42b1b88478dc481a5d325251d7b63fd
William Nollert <will.nollert@gmail.com>
2022-01-10 10:33:35 -0600

    added login/signup route

--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,21 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
+import NotFound from "./containers/NotFound";
+import Login from "./containers/Login";
+
+
 
 export default function Routes() {
   return (
     <Switch>
       <Route exact path="/">
         <Home />
-      </Route>
+        </Route>
+        <Route exact path ="/Login">
+            <Login />
+        </Route>
+        <NotFound />
     </Switch>
   );
 }
