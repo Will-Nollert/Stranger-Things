74069341cd88ff2bd224f9d60f8a675055b50d9d
William Nollert <will.nollert@gmail.com>
2022-01-10 10:26:23 -0600

    inital commit after some work on new track, working on login route, stable stop

---
+++ src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>Stranger's Things</h1>
+        <p className="text-muted">a simple craigslist clone</p>
+      </div>
+    </div>
+  );
+}
