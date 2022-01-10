7926816c2913325c3894199a4de9429b956bff5b
William Nollert <will.nollert@gmail.com>
2022-01-10 10:59:31 -0600

    added signUp page,route, and styled-- stable stop-- needs back end hookup

--- src/Routes.js
+++ src/Routes.js
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
 import Login from "./containers/Login";
+import SignUp from "./containers/SignUp";
 
 
 
@@ -15,6 +16,9 @@ export default function Routes() {
         <Route exact path ="/Login">
             <Login />
         </Route>
+        <Route exact path="/Signup">
+  <SignUp />
+</Route>
         <NotFound />
     </Switch>
   );
